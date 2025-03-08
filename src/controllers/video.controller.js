6082ea27c96ea4d08c624cabbc7d1c2f6508395e
umarfarooq3488 <uf88605@gmail.com>
2025-03-08 16:29:00 +0500

    Implement comment and tweet controllers, add pagination to video retrieval, and toggle video publish status

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -3,7 +3,6 @@ import { Video } from "../models/Video.model.js"
 import { ApiError } from "../utils/ApiErrors.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 import uploadFileOnCloudinary from "../utils/Cloudinary.js";
-import { User } from "../models/User.model.js"
 
 
 const uploadVideo = asyncHandler(async (req, res) => {
@@ -104,7 +103,13 @@ const deleteVideo = asyncHandler(async (req, res) => {
 })
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const allVideos = await Video.find().populate("owner", "userName fullName avatar")
+    const { page = 1, limit = 10 } = req.query
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10),
+        populate: { path: "owner", select: "userName fullName avatar" }
+    }
+    const allVideos = await Video.paginate({}, options)
     if (!allVideos) {
         throw new ApiError(401, "There is a problem while fetching the videos")
     }
@@ -113,17 +118,31 @@ const getAllVideos = asyncHandler(async (req, res) => {
 })
 
 const searchVideos = asyncHandler(async (req, res) => {
-    const { query } = req.query;
+    const { query, page = 1, limit = 1, sortBy, sortType, userId } = req.query;
+    const filter = {}
     if (!query) {
         throw new ApiError(401, "Search query is not provided")
+    } else {
+        filter.title = query
+    }
+    if (userId) {
+        filter.owner = userId
     }
 
-    const videos = await Video.find({
-        $or: [
-            { title: { $regex: query, $options: 'i' } },
-            { description: { $regex: query, $options: 'i' } }
-        ]
-    }).populate("owner", "userName fullName avatar")
+    const sortOptions = {}
+    if (sortBy) {
+        sortOptions[sortBy] = sortType === "desc" ? -1 : 1;
+    }
+
+
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10),
+        sort: sortOptions,
+        populate: { path: "owner", select: "fullName userName avatar" }
+    }
+
+    const videos = await Video.paginate(filter, options)
     if (!videos) {
         throw new ApiError(400, "No videos matched")
     }
@@ -132,11 +151,34 @@ const searchVideos = asyncHandler(async (req, res) => {
     )
 })
 
+const togglePublishVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+
+    const video = await Video.findByIdAndUpdate(videoId,
+        [
+            {
+                $set: {
+                    isPublished: { $not: "$isPublished" }
+                }
+            }
+        ], {
+        new: true
+    }
+    )
+
+    res.status(200).json(
+        new ApiResponse(200, video, "Video status is changed successfully")
+    )
+})
+
+
+
 export {
     uploadVideo,
     getVideoDetails,
     updateVideoDetails,
     deleteVideo,
     getAllVideos,
-    searchVideos
+    searchVideos,
+    togglePublishVideo
 }
